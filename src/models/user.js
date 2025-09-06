e7bbfc2152203098b18c7b91752fc4de641b01ed
Petrando <petrandorichard@gmail.com>
2025-09-06 13:14:59 +0800

    update USERS data with the newest metadata, re-enable existing user check when registering

--- src/models/user.js
+++ src/models/user.js
@@ -7,37 +7,38 @@ export const UserRole = {
 export const USERS = [
   {
     email: 'client@galaxies.dev',
-    id: '7z6ydcm',
+    id: 'mbz1coo',
     role: UserRole.Client,
     hashed_password: '$2b$10$.MftzcPPsR5TUTYRyWGyQu9H9Fd3Q6olBlccI1qIAY3qXH7OQ.bQO', // dummy "123456"
   },
+  // for the rest of the users, the password is "pass123456"
   {
     email: 'simon@galaxies.dev',
-    id: '2prkds5',
+    id: 'vv6s6mt',
     role: UserRole.Therapist,
     hashed_password: '$2b$10$ZqK7Gm8f3pL9yT4xC2n5Jew5ejjR0N8kT2oNM8iJH0bFmqs9YHRTm', 
   },
   {
-    email: 'petrando@gmail.com',
-    id: 'lh74jhe',
-    role: UserRole.Therapist,
-    hashed_password: '$2b$10$ZqK7Gm8f3pL9yT4xC2n5Jew5ejjR0N8kT2oNM8iJH0bFmqs9YHRTm'
-  },
-  {
     email: 'petrando1@gmail.com',
-    id: 'x0rl5vi',
+    id: 'sk2igl3',
     role: UserRole.Therapist,
     hashed_password: '$2b$10$ZqK7Gm8f3pL9yT4xC2n5Jew5ejjR0N8kT2oNM8iJH0bFmqs9YHRTm'
   },
   {
-    email: 'client@gmail.com',
-    id: 'u6doe6w',
-    role: UserRole.Client,
+    email: 'petrando2@gmail.com',
+    id: 'lskmkmc',
+    role: UserRole.Therapist,
     hashed_password: '$2b$10$ZqK7Gm8f3pL9yT4xC2n5Jew5ejjR0N8kT2oNM8iJH0bFmqs9YHRTm'
   },
   {
     email: 'client1@gmail.com',
-    id: 's26dnij',
+    id: 'clr0j6q',
+    role: UserRole.Client,
+    hashed_password: '$2b$10$ZqK7Gm8f3pL9yT4xC2n5Jew5ejjR0N8kT2oNM8iJH0bFmqs9YHRTm'
+  },
+  {
+    email: 'client2@gmail.com',
+    id: 'vpmwlbx',
     role: UserRole.Client,
     hashed_password: '$2b$10$ZqK7Gm8f3pL9yT4xC2n5Jew5ejjR0N8kT2oNM8iJH0bFmqs9YHRTm'
   }
